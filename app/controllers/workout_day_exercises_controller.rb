c2cff4cb8925f5fe9d53c8eb1d6e3ee317d32cac
laaksom <laakso.mavrick@gmail.com>
2022-06-05 22:42:32 -0400

    Create workout day exercise form implementation

--- app/controllers/workout_day_exercises_controller.rb
+++ app/controllers/workout_day_exercises_controller.rb
@@ -2,5 +2,23 @@
 
 class WorkoutDayExercisesController < ApplicationController
   def new
+    workout_id = params[:workout_id]
+    workout_day_id = params[:workout_day_id]
+
+    @workout = Workout.find(workout_id)
+    @workout_day = WorkoutDay.find(workout_day_id)
+
+    @exercises = current_user.exercises.map { |e| [e.name, e.id ]}
+    @sets = [*1..5]
+    @reps = [*1..15]
+  end
+
+  def create
+
+  end
+
+  private
+
+  def new_workout_day_exercise_params
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,14 @@ module ApplicationHelper
     'max-w-4xl mx-auto px-2 sm:px-6 lg:px-8'
   end
 
+  def default_button_class(css_class_string = "")
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer #{css_class_string}"
+  end
+
+  def create_button_class(css_class_string = "")
+    "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer #{css_class_string}"
+  end
+
   def nav_link_to(text, path, opts = {})
     is_active = current_page?(path)
     opts[:class] ||= "#{is_active ? 'text-black-700' : 'text-gray-500'} px-3 py-2 rounded-md text-sm font-medium"
