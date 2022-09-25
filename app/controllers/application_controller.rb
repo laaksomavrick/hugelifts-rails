e1148771f6b730bbf5e888c007197068613fd091
Mavrick Laakso <laakso.mavrick@gmail.com>
2022-09-25 18:46:33 -0400

    Disable rubocop Rails/LexicallyScopedActionFilter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   before_action :authenticate_user!
 
+  # rubocop:disable Rails/LexicallyScopedActionFilter
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
+  # rubocop:enable Rails/LexicallyScopedActionFilter
 
   # Here so Rubymine stops complaining
   def current_user # rubocop:disable Lint/UselessMethodDefinition
