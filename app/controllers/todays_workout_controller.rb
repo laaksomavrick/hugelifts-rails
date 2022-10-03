af6f4d0878f657547f2886d82a14b02a423a2dde
Mavrick Laakso <laakso.mavrick@gmail.com>
2022-10-02 20:39:30 -0400

    Extracted TodaysWorkoutService from controller

--- app/controllers/todays_workout_controller.rb
+++ app/controllers/todays_workout_controller.rb
@@ -1,91 +1,11 @@
 # frozen_string_literal: true
 
-#
 # TODO: on completion of an exercise, increase the weight
 
 class TodaysWorkoutController < ApplicationController
   def index
-    # policy_scope(nil, policy_scope_class: TodaysWorkoutPolicy::Scope)
-    # Find the appropriate scheduled_workout
-    # If none exists, create from ordinal 0 of the active workout
-    # If some exist:
-    #   If one is incomplete, return that
-    #   If none are incomplete, find the latest completion ordinal and create from ordinal + 1 (cycle)
-
-    # TODO: extract to service object
+    # TODO: policy_scope(nil, policy_scope_class: TodaysWorkoutPolicy::Scope)
     # TODO: todays_workout_presenter
-    # TODO: transaction
-    # TODO: test
-    # TODO: system test (it renders todays workout)
-
-    active_workout = current_user.workouts.active_workout_for_user(current_user)
-
-    if active_workout == false
-      @todays_workout = nil
-      return
-    end
-
-    active_workout_days = active_workout.days
-    active_workout_day_ids = active_workout_days.map(&:id)
-    has_scheduled_workout = current_user.scheduled_workouts.where(workout_day_id: [active_workout_day_ids]).any?
-
-    if has_scheduled_workout == false
-      to_schedule_workout_day = active_workout_days.order(ordinal: :asc).first
-
-      # TODO: dry
-      todays_workout = ScheduledWorkout.new(completed: false, workout_day: to_schedule_workout_day, user: current_user)
-      todays_workout.save!
-
-      to_schedule_workout_day.exercises.each do |exercise|
-        scheduled_exercise = ScheduledWorkoutExercise.new(
-          sets: exercise.sets,
-          reps: exercise.reps,
-          weight: exercise.weight,
-          result: [],
-          scheduled_workout: todays_workout,
-          workout_day_exercise: exercise
-        )
-        scheduled_exercise.save!
-      end
-
-      @todays_workout = ScheduledWorkout.includes(:scheduled_workout_exercises, :workout_day).find(todays_workout.id)
-    else
-      incomplete_workout = current_user.scheduled_workouts
-                                       .includes(:scheduled_workout_exercises, :workout_day)
-                                       .where(completed: false, workout_day: [active_workout_day_ids])
-                                       .first
-
-      if incomplete_workout.present?
-        @todays_workout = incomplete_workout
-      else
-        last_completed_workout = current_user.scheduled_workouts
-                                             .includes(:workout_day)
-                                             .where(completed: true, workout_day: [active_workout_day_ids])
-                                             .order(created_at: :desc)
-                                             .first
-        ordinals = last_completed_workout.workout_day.workout.workout_days.order(ordinal: :asc).map(&:ordinal)
-        last_completed_ordinal = last_completed_workout.workout_day.ordinal
-        next_ordinal = last_completed_ordinal == ordinals.last ? ordinals.first : last_completed_ordinal + 1
-        next_workout_day = active_workout_days.where(ordinal: next_ordinal).first
-
-        # TODO: DRY
-        todays_workout = ScheduledWorkout.new(completed: false, workout_day: next_workout_day, user: current_user)
-        todays_workout.save!
-
-        next_workout_day.exercises.each do |exercise|
-          scheduled_exercise = ScheduledWorkoutExercise.new(
-            sets: exercise.sets,
-            reps: exercise.reps,
-            weight: exercise.weight,
-            result: [],
-            scheduled_workout: todays_workout,
-            workout_day_exercise: exercise
-          )
-          scheduled_exercise.save!
-        end
-
-        @todays_workout = ScheduledWorkout.includes(:scheduled_workout_exercises, :workout_day).find(todays_workout.id)
-      end
-    end
+    @todays_workout = TodaysWorkoutService.new(user: current_user).call
   end
 end
---
+++ app/services/todays_workout_service.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+class TodaysWorkoutService
+  attr_reader :user
+
+  def initialize(user:)
+    @user = user
+  end
+
+  def call
+    ScheduledWorkout.transaction do
+      active_workout = @user.workouts.active_workout_for_user(@user)
+
+      raise "No active workout for user=#{@user.id}" if active_workout == false
+
+      active_workout_days = active_workout.days
+      active_workout_day_ids = active_workout_days.map(&:id)
+
+      scheduled_workout = scheduled_workout(active_workout_day_ids:)
+      incomplete_workout = incomplete_workout(active_workout_day_ids:)
+      last_completed_workout = last_completed_workout(active_workout_day_ids:)
+
+      if scheduled_workout.none?
+        to_schedule_workout_day = active_workout_days.order(ordinal: :asc).first
+        generate_todays_workout(workout_day: to_schedule_workout_day)
+      elsif incomplete_workout.present?
+        incomplete_workout
+      elsif last_completed_workout.present?
+        ordinals = last_completed_workout.workout_day.workout.workout_days.order(ordinal: :asc).map(&:ordinal)
+        last_completed_ordinal = last_completed_workout.workout_day.ordinal
+        next_ordinal = last_completed_ordinal == ordinals.last ? ordinals.first : last_completed_ordinal + 1
+
+        next_workout_day = active_workout_days.where(ordinal: next_ordinal).first
+        generate_todays_workout(workout_day: next_workout_day)
+      else
+        raise "Something unexpected went wrong generating a scheduled_workout for workout=#{active_workout.id}"
+      end
+    end
+  end
+
+  private
+
+  def last_completed_workout(active_workout_day_ids:)
+    @user.scheduled_workouts
+         .includes(:workout_day)
+         .where(completed: true, workout_day: [active_workout_day_ids])
+         .order(created_at: :desc)
+         .first
+  end
+
+  def incomplete_workout(active_workout_day_ids:)
+    @user.scheduled_workouts
+         .includes(:scheduled_workout_exercises, :workout_day)
+         .where(completed: false, workout_day: [active_workout_day_ids])
+         .first
+  end
+
+  def scheduled_workout(active_workout_day_ids:)
+    @user.scheduled_workouts.where(workout_day_id: [active_workout_day_ids])
+  end
+
+  def generate_todays_workout(workout_day:)
+    todays_workout = ScheduledWorkout.new(completed: false, workout_day:, user: @user)
+    todays_workout.save!
+
+    workout_day.exercises.each do |exercise|
+      scheduled_exercise = ScheduledWorkoutExercise.new(
+        sets: exercise.sets,
+        reps: exercise.reps,
+        weight: exercise.weight,
+        result: [],
+        scheduled_workout: todays_workout,
+        workout_day_exercise: exercise
+      )
+      scheduled_exercise.save!
+    end
+
+    ScheduledWorkout.includes(:scheduled_workout_exercises, :workout_day).find(todays_workout.id)
+  end
+end
