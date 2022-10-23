8ea39ce8a4b16b9e68fe7bf2499f73312ead8bac
Mavrick Laakso <laakso.mavrick@gmail.com>
2022-10-23 23:38:05 +0000

    Updated certbot + nginx config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(hugelifts.ca www.hugelifts.ca)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="laakso.mavrick@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose up --force-recreate -d reverse-proxy
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
