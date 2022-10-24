24f98167257ba099a5f64383fc6f4f8e04c2fa13
Mavrick Laakso <laakso.mavrick@gmail.com>
2022-10-24 08:09:37 -0400

    Fix for site manifest 404

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     todays_workout_index_path
   end
 end
