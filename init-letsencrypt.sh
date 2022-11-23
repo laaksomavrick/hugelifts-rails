e608dbbb5a1834c3cea7319f0704872431e91b25
Mavrick Laakso <laakso.mavrick@gmail.com>
2022-11-22 21:20:50 -0500

    Attempted SSL setup for admin.hugelifts.ca

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(hugelifts.ca www.hugelifts.ca)
+domains=(hugelifts.ca www.hugelifts.ca admin.hugelifts.ca www.admin.hugelifts.ca)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="laakso.mavrick@gmail.com" # Adding a valid address is strongly recommended
